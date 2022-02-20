8fa15ed49b753e1bd6809d344d92b9b01d44992d
Max Schenkelberg <max@papiocloud.com>
2022-02-20 14:13:00 -0600

    fix(machineimagelookup): making hash for singleton deterministic by resolving tokens first

--- lib/ec2/MachineImageLookup.ts
+++ lib/ec2/MachineImageLookup.ts
@@ -7,7 +7,8 @@ import {
 } from "@aws-cdk/aws-ec2";
 import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
 import { Code, Runtime, SingletonFunction } from "@aws-cdk/aws-lambda";
-import { Construct, CustomResource, Duration } from "@aws-cdk/core";
+import { Construct, CustomResource, Duration, ITokenResolver, Token, Tokenization } from "@aws-cdk/core";
+import { CLOUDFORMATION_TOKEN_RESOLVER } from "@aws-cdk/core/lib/private/cloudformation-lang";
 import { Provider } from "@aws-cdk/custom-resources";
 import * as crypto from "crypto";
 import * as path from "path";
@@ -24,7 +25,11 @@ export class MachineImageLookup implements IMachineImage {
 
   getImage(scope: Construct): MachineImageConfig {
     // Create a unique hash to avoid scope child id conflict, if same props should be same object
-    const id = crypto.createHash("sha256").update(JSON.stringify(this.props)).digest("hex").substring(0, 6);
+    const resolved = Tokenization.resolve(JSON.stringify(this.props), {
+      scope,
+      resolver: CLOUDFORMATION_TOKEN_RESOLVER
+    });
+    const id = crypto.createHash("sha256").update(JSON.stringify(resolved)).digest("hex").substring(0, 6);
 
     const lookupFunction = new SingletonFunction(scope, `MachineImageLookupFunction_${id}`, {
       code: Code.fromAsset(path.resolve(__dirname, "..", "lambdas")),
